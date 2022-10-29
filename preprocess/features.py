11843ae013262c58b9e076311f12fbc71bf1e00e
Dekel Cohen <dekelcohen@gmail.com>
2022-10-30 01:25:00 +0300

    Added persons chars_aug/train.txt valid.txt test.txt

--- preprocess/features.py
+++ preprocess/features.py
@@ -9,23 +9,18 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
-folder_path = './temp'
 
 def format_cand(cand_txt, add_chars = False):
     cand_flds = f"COL name VAL {cand_txt}"
-    if add_chars_aug:
+    if add_chars:
         cand_flds += f"\tCOL name_chars VAL {' '.join(list(cand_txt))}"
     return cand_flds
 
-def write_split(file_name, df, add_chars= False):    
-    save_ents_df(df, Path(folder_path) / (str(Path(file_name).stem) +  '.parquet'))        
+def write_split(folder_path, file_name, df, add_chars= False):    
+#    save_ents_df(df, Path(folder_path) / (str(Path(file_name).stem) +  '.parquet'))        
     with open(Path(folder_path) / file_name, 'w', encoding = 'utf-8') as f:
         for rec_index, row in df.iterrows():
             f.write(f"{format_cand(row['tokens_str'], add_chars)}\t{format_cand(row['m_tokens_str'], add_chars)}\t{row['gt_label']}\n")
-
-write_split('train.txt', df_train)
-write_split('valid.txt', df_val)
-write_split('test.txt', df_test)
 
 
 def read_split(txt_file_path):
@@ -33,17 +28,25 @@ def read_split(txt_file_path):
         rows = fl.readlines()
     return rows
     
-def add_chars_split(txt_file_path, file_name):
+def add_chars_split(folder_path, file_name):
     rows = read_split(Path(folder_path) / file_name)
+    tokens_str = []
+    m_tokens_str = []
+    gt_label = []
+    
     for row in rows:
-        flds = rows[0].split('\t')
+        flds = row.split('\t')
         cand1 = flds[0].split('COL name VAL ')[1]
+        tokens_str.append(cand1)
         cand2 = flds[1].split('COL name VAL ')[1]
-        gt_label = flds[2].strip()
-        
-    write_split(Path(folder_path) / 'chars_aug' / file_name)
+        m_tokens_str.append(cand2)
+        gt_label.append(flds[2].strip())
+    df = pd.DataFrame(data={'tokens_str': tokens_str, 'm_tokens_str': m_tokens_str, 'gt_label' :gt_label })    
+    write_split(Path(folder_path) / 'chars_aug', file_name, df, add_chars=True)
     
 def add_chars_dataset(folder_path):    
+    add_chars_split(folder_path, 'train.txt')
+    add_chars_split(folder_path, 'valid.txt')
     add_chars_split(folder_path, 'test.txt')
     
 

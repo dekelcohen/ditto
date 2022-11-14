397c283d8a4edcae6bd95d6bff4591fca28ade02
Dekel Cohen <dekelc@harmon.ie>
2022-11-14 13:02:23 +0200

    Google trasnslate doesn't work well for lowercased names Solution: capitalize every first word

--- preprocess/trans.py
+++ preprocess/trans.py
@@ -1,8 +1,15 @@
 from translate.translate_ds import TranslateUtil
 from features import read_split_df
+import string
 
+def read_split_df_to_camelcase(path):
+     df = read_split_df(path)
+     df['tokens_str'] = df['tokens_str'].apply(lambda x: string.capwords(x))
+     df['m_tokens_str'] = df['m_tokens_str'].apply(lambda x: string.capwords(x))
+     return df
+     
 def translate_splits(folder_path, file_names = ['train.txt','valid.txt','test.txt']):
-     trutil = TranslateUtil(folder_path, file_names,tr_cols = ['tokens_str','m_tokens_str'],copy_cols = ['gt_label'],read_df_func = read_split_df)
+     trutil = TranslateUtil(folder_path, file_names,tr_cols = ['tokens_str','m_tokens_str'],copy_cols = ['gt_label'],read_df_func = read_split_df_to_camelcase)
      trutil.save_eng_xls()  
 
 

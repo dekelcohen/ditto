a61e517ca47d64841200d4b0ecae1865c32bc764
Dekel Cohen <dekelc@harmon.ie>
2022-11-14 21:30:40 +0200

    Didn't finish translation of Ditto for now

--- preprocess/trans.py
+++ preprocess/trans.py
@@ -4,16 +4,20 @@ from translate.translate_ds import TranslateUtil
 from features import read_split_df
 # Depends on nlp-util in PYTHONPATH
 
-def read_split_df_to_camelcase(path):
+# Set working directory: cd /d D:\Dekel\Data\NLP\EntityMatching\ditto\preprocess
+
+def read_split_df_first_letter_upper_case(path):
      df = read_split_df(path)
      df['tokens_str'] = df['tokens_str'].apply(lambda x: string.capwords(x))
      df['m_tokens_str'] = df['m_tokens_str'].apply(lambda x: string.capwords(x))
      return df
      
 def translate_splits(folder_path, file_names):
-     trutil = TranslateUtil(folder_path, file_names, tr_cols = ['tokens_str','m_tokens_str'], copy_cols = ['gt_label'], read_df_func = read_split_df_to_camelcase)
+     trutil = TranslateUtil(folder_path, file_names, tr_cols = ['tokens_str','m_tokens_str'], copy_cols = ['gt_label'], read_df_func = read_split_df_first_letter_upper_case)
+     # 1) Save .xlsx files with eng, ready to upload
      trutil.save_eng_xls()
-     # Now - upload, manually, the .xlsx files from eng_for_tr folder created in prev step --> save translations to ar
+     # 2) Manually upload to Google Translate , the .xlsx files from eng_for_tr folder created in prev step --> save translations to ar
+     # 3) create final files with column names in eng + copied columns from eng .xlsx
      trutil.process_tr_files(Path(folder_path) / 'ar')
 
 
